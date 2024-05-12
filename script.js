91e2b4e8b080726e755f6aeed6a6ac7ad4e22e19
void-the-droid <bhuwan626282@gmail.com>
2024-05-12 23:03:25 +0545

    learned things or two

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//DOM manipulation 
+ 
+const clrBtn = document.getElementById("clearBtn")
+const colorBtn = document.getElementById("colorBtn")
+
+const main = document.querySelector(".main-game")
+
+// main.backgroundColor="rgb(" +random1 + "," + random2 + ", " + random3 + ")";
+
+
+
 const defaultSize =10;
 
 const grid =document.querySelector('.grid')
@@ -8,15 +19,39 @@ var size= defaultSize
   grid.style.gridTemplateRows=`repeat(${size},1fr)`;  
 
 
+ 
+  
+ 
 
 for(let i=0; i<size*size; i++){
     const gridItems = document.createElement('div')
     gridItems.classList.add('grid-items')
 
     gridItems.addEventListener('mouseover',function(){
-        gridItems.style.backgroundColor='pink'
+        gridItems.style.backgroundColor='#6b6b6b'
     })
 
     grid.appendChild(gridItems)
 
+    clrBtn.addEventListener('click',()=>{
+      gridItems.style.backgroundColor=''
+    })
+
+    //Random RBG value for random color creation
+    let red= Math.floor((Math.random())*256)
+    let green= Math.floor((Math.random())*256)
+    let blue= Math.floor((Math.random())*256)
+
+    //Color button action , which will provide randomness in etchy sektch palate
+    colorBtn.addEventListener('click',()=>{
+      gridItems.addEventListener('mouseover',()=>{
+        gridItems.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+      })
+    })
+
 }
+
+
+
+
+console.log(random2,random1,random3)
