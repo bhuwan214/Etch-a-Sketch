fe4519b8f475ea95e69ece5bc4db9aba8cd1c9a0
void-the-droid <bhuwan626282@gmail.com>
2024-05-15 07:06:46 +0545

    restructuring

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //DOM manipulation
 
 const clrBtn = document.getElementById("clearBtn");
-const colorBtn = document.getElementById("colorBtn");
-const color = document.querySelector('input[type="color"]');
+const RandomcolorBtn = document.getElementById("colorBtn");
+const Color = document.querySelector('input[type="color"]');
 const rangeInput = document.querySelector('input[type="range"]');
 const rangeValue = document.querySelector("#value")
 const Eraser = document.querySelector("img");
@@ -30,60 +30,69 @@ for (let i = 0; i < size * size; i++) {
 //creating the grid-item and  making them child of grid
   const gridItems = document.createElement("div");
   gridItems.classList.add("grid-items");
-
+  grid.appendChild(gridItems);
 
   //coloring the grid items
   gridItems.addEventListener("mouseover", function () {
-    gridItems.style.backgroundColor = `${color.value}`;
+    gridItems.style.backgroundColor = `${Color.value}`;
   });
 
-  grid.appendChild(gridItems);
-
+  //Reseting the color of grid-items to white
   clrBtn.addEventListener("click", () => {
     gridItems.style.backgroundColor = "";
-
     gridItems.addEventListener("mouseover", () => {
-      gridItems.style.backgroundColor = `${color.value}`
-
-    });
+        gridItems.style.backgroundColor = `${Color.value}`
+  
+      });
 
   });
 
+  //Erasing the color items
   Eraser.addEventListener("click", () => {
     gridItems.addEventListener("mouseover", () => {
-      gridItems.style.backgroundColor = "white"
-
+      gridItems.style.backgroundColor = ""
     });
-
   });
 
 
   //Random RBG value for random color creation
+  
+
+  //Color button action , which will provide randomness in etchy sektch palate
+  RandomcolorBtn.addEventListener("click", ()=>{
+    gridItems.addEventListener('mouseover', setRandomColor)
+  } );
+
+  //Coloring the component (fix for random click not working default coloring function )
+    Color.addEventListener("click", () => {
+     let defaultColor=settingColor()
+     gridItems.addEventListener('mouseover', ()=>{
+    gridItems.style.backgroundColor=`${Color.value}`;
+
+     }
+    )
+    });
+}
+
+//Generattng random color 
+function RandomColor(){
   let red = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let blue = Math.floor(Math.random() * 256);
 
-  //Color button action , which will provide randomness in etchy sektch palate
-  colorBtn.addEventListener("click", () => {
-    gridItems.addEventListener("mouseover", () => {
-      gridItems.style.backgroundColor =
-        "rgb(" + red + ", " + green + ", " + blue + ")";
-    });
-  });
+  return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
 
-  //Coloring the component (fix for random click not working default coloring function )
-
-    color.addEventListener("click", () => {
-      gridItems.addEventListener("mouseover", () => {
-        gridItems.style.backgroundColor = `${color.value}`
-  
-      });
-    });
-
-  //Erasing the colored components
-  
+//Appling the random color to grid items
+function setRandomColor(){
+  const randomColor =RandomColor()
+    this.style.backgroundColor =randomColor
+}
 
 
+//getting color
+function settingColor(){
+return `${Color.value}`
 }
 
 function refreshPage(){
